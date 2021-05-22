b6d747e6a2a23604dc89dc0770f1d1a218c03419
scpepper <secret.crunch.pepper@gmail.com>
2021-05-22 17:12:01 +0900

    Update 01_extract_image_from_gif.py

--- bin/01_extract_image_from_gif.py
+++ bin/01_extract_image_from_gif.py
@@ -2,8 +2,8 @@ import sys
 from pathlib import Path
 from PIL import Image, ImageSequence
 
-IMAGE_PATH  = sys.argv[1]  #'./booster.gif'
-DESTINATION = sys.argv[2]  #'./'
+gif_path  = sys.argv[1]  #'./booster.gif'
+dest_path = sys.argv[2]  #'./'
 
 DEBUG_MODE = True
 
@@ -11,28 +11,28 @@ def get_frames(path):
     im = Image.open(path)
     return (frame.copy() for frame in ImageSequence.Iterator(im))
 
-def write_frames(frames, name_original, destination):
+def write_frames(frames, name_original, dest_path):
     path = Path(name_original)
 
     stem = path.stem
     #extension = path.suffix
     extension = '.png'
-    destination = destination + stem
+    dest_path = dest_path + stem
 
-    dir_dest = Path(destination)
+    dir_dest = Path(dest_path)
     if not dir_dest.is_dir():
         dir_dest.mkdir(0o700)
         if DEBUG_MODE:
-            print('Destionation directory is created: "{}".'.format(destination))
+            print('Destionation directory is created: "{}".'.format(dest_path))
 
     for i, f in enumerate(frames):
-        name = '{}/{}-{}{}'.format(destination, stem, str(i + 1).zfill(2), extension)
+        name = '{}/{}-{}{}'.format(dest_path, stem, str(i + 1).zfill(2), extension)
         f.save(name)
         if DEBUG_MODE:
             print('A frame is saved as "{}".'.format(name))
 
 
-frames = get_frames(IMAGE_PATH)
-write_frames(frames, IMAGE_PATH, DESTINATION)
+frames = get_frames(gif_path)
+write_frames(frames, gif_path, dest_path)
 
 exit()
